f783ee82cd534bb8f095af1a1d505409cab3348c
ajitkaurav <33899040+ajitkaurav@users.noreply.github.com>
2024-12-06 14:41:04 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Ajit !";
+        return "Hello from AjitSingh !";
     }   
 }
